7d7a914d88440c1d8a2da739f25e6483cd3a6379
Jatin Bhardwaj <bhardwajjatin093@gmail.com>
2025-01-29 19:55:45 +0530

    remove blog for the time bieng

--- src/components/Blogs.vue
+++ src/components/Blogs.vue
@@ -21,7 +21,7 @@ const BlogList = ref<Blog[]>([
         <div class="mx-auto w-[70vw] mt-24">
             <div class="flex flex-col items-center gap-7">
                 <!-- Blogs List -->
-                <div v-for="(blog, index) in BlogList" :key="index" class="mb-6">
+                <!-- <div v-for="(blog, index) in BlogList" :key="index" class="mb-6">
                     <a :href="blog.link">
                         <div
                             class="w-[60vw] h-60 mx-auto p-4 flex flex-col items-center justify-center bg-purple-500 rounded-lg hover:bg-purple-600 transition ease-in-out duration-300 transform hover:scale-105 cursor-pointer">
@@ -29,11 +29,12 @@ const BlogList = ref<Blog[]>([
                                 {{ blog.heading }}
                             </p>
                             <p class="lg:text-lg text-white mt-4 md:text-md sm:text-sm">
-                                {{ blog.content }} ---->
+                                {{ blog.content }}
                             </p>
                         </div>
                     </a>
-                </div>
+                </div> -->
+                <p class="text-3xl font-bold font-ubuntu text-black" >Work in progress</p>
             </div>
         </div>
     </div>
